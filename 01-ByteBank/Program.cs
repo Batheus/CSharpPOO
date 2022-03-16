f2759fbd2b8ed25a298e419faea7cc5df3e7db51
Matheus <batheus.dev@gmail.com>
2022-03-16 20:59:05 -0300

    01_02

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 123;
+            contaDaGabriela.numero = 123456;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+
+            Console.WriteLine("Novo saldo: " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02-ByteBank/ContaCorrente.cs
@@ -0,0 +1,7 @@
+﻿public class ContaCorrente
+{
+    public string titular;
+    public int agencia = 17;
+    public int numero;
+    public double saldo;
+}
