8bd08cf00d6fa5ebd7605129b28c4cb431f51fa8
Matheus <batheus.dev@gmail.com>
2022-04-04 17:43:39 -0300

    05_06

---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Anderson";
+            cliente.CPF = "123.456.789-10";
+            cliente.Profissao = "Professor";
+
+            conta.Saldo = 700;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            ContaCorrente conta = new ContaCorrente(867, 89879456);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            ContaCorrente contadoMario = new ContaCorrente(867, 89879470);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+        }
+    }
+}
